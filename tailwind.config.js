087af3b150a94bb087cc7f973d1a69ee830558d1
madhavavarma.m@gmail.com <madhavavarma.m@gmail.com>
2024-11-21 14:29:30 +0530

    Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Include JSX, TSX, JS, and TS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
